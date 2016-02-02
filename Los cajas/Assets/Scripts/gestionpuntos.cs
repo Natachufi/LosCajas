49af99238d33a9e9485e16cedcc80c306f3c5c31
Luciana <LV>
2016-02-02 20:28:18 +0100

    #7 Script de puntuacion

---
+++ Los cajas/Assets/Scripts/gestionpuntos.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gestionpuntos : MonoBehaviour {
+
+	//poner script a jugador
+
+	public Text textopuntos; // vincular el texto que mostrara los puntos
+	public int puntos = 0; 
+
+	public void sumapuntos(int nuevospuntos){
+	
+		puntos = puntos + nuevospuntos;
+		textopuntos.text = " " + puntos;
+	
+	}
+
+}
