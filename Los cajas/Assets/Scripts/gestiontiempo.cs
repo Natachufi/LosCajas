f939710a57718fd79d52484c1755e78c1f55691d
Luciana <LV>
2016-02-03 17:01:02 +0100

    8. Creacion de script tiempo

---
+++ Los cajas/Assets/Scripts/gestiontiempo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gestiontiempo : MonoBehaviour {
+
+	public float minutos;
+	public float segundos;
+
+	public Text contador;
+
+    //public float segundos, minutos;
+
+	// Use this for initialization
+	void Start () {
+
+		contador = GetComponent<Text>() as Text;	
+	
+
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+		minutos = (Time.timeSinceLevelLoad / 60f);
+
+		segundos = (Time.timeSinceLevelLoad % 60f);
+
+		contador.text = minutos.ToString("00") + ":" + segundos.ToString("00");
+
+
+	}
+}
