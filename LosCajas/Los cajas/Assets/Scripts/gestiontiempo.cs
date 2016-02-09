9a3051862affbfe37ef4f859e9ea3ba90fb0724a
Luciana <LV>
2016-02-09 17:09:33 +0100

    #8 Creacion script tiempo

---
+++ LosCajas/Los cajas/Assets/Scripts/gestiontiempo.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gestiontiempo : MonoBehaviour {
+
+	public float minutos;
+	public float segundos;
+	public float penalizacion;
+	public Text contador;
+
+	//añadir este script al texto del contador en la iu   
+
+
+	void Start () {
+
+		contador = GetComponent<Text>() as Text;
+	}
+	
+
+	public void Update () {
+
+		tiempo();  
+	}
+
+
+	void tiempo (){
+
+		minutos = (Time.timeSinceLevelLoad / 60f);
+		
+		segundos = (Time.timeSinceLevelLoad % 60f)+penalizacion;
+
+		contador.text = minutos.ToString("00") + ":" + segundos.ToString("00");
+	
+	}
+
+
+	public void tiempoextra(float segundosextra){
+
+		Debug.Log 	(segundosextra);
+		penalizacion +=	segundosextra % 60f;
+	
+	}
+
+}
---
+++ LosCajas/Los cajas/Assets/Scripts/tiempo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tiempo : MonoBehaviour {
+
+	private gestiontiempo gt;
+	public float segundosextra;
+	public bool unavez = false;
+
+	//todas las banderillas deberan tener dos collider  que sobresalgan en los extremos con este script incorporado
+
+	void Start () {
+		gt = GameObject.Find ("Text").GetComponent<gestiontiempo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider objeto){ 
+		
+		if (objeto.tag == "Player" && !unavez){
+			Debug.Log ("personaje dentro " + segundosextra);
+			gt.tiempoextra(segundosextra);
+			unavez = true;
+		}
+		
+	}
+	void OnTriggerExit(Collider objeto){
+		if (objeto.tag == "Player"){
+			unavez = false;
+		}
+	}
+
+}
