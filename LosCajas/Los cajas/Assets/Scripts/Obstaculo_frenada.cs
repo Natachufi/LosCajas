d3439e07fdf0bc556de95ecad3748356b1fbe740
JoseTankian <jose.ma.16@hotmail.com>
2016-02-09 19:00:38 +0100

    [38] [35] Revision

---
+++ LosCajas/Los cajas/Assets/Scripts/Obstaculo_frenada.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstaculo_frenada : MonoBehaviour 
+{
+	bool obstaculo = false;
+	Rigidbody rg;
+	
+	void Start()
+	{
+		rg = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+	{
+		if(obstaculo)
+		{
+			rg.AddForce(transform.forward/2000);
+			obstaculo=false;
+		}	
+	}
+	
+	void OnCollisionEnter(Collision objeto)
+	{
+		if(objeto.transform.tag == "obstaculo")
+		{
+			obstaculo=true;
+		}
+	}
+}
