fd00ea436605ac9a4df7c68c3dc6aab94e09546c
IreneAguayo <irene_aguayo93@hotmail.com>
2016-02-16 19:47:05 +0100

    [55] unificar scripts

--- LosCajas/Los cajas/Assets/Scripts/Movimiento.cs
+++ LosCajas/Los cajas/Assets/Scripts/Movimiento.cs
@@ -1,54 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movimiento : MonoBehaviour {
+public class movimiento : MonoBehaviour {
+	//debe coger el valor z de la camara y convertirlo en un valor y en este objeto que determinara hacia donde gira el objeto
+	// debe tener un rozamiento 00 y solo mueve de izq a derecha
+	Rigidbody rg;
+	public Camara_giros cg;
+	public float ladeado;
+	public Vector3 izq;
+	//public GameObject ski1, ski2;
 
-	public float accel = 100f;
-	Rigidbody rb;
-	public float giro;
-
-	// Use this for initialization
 	void Start () {
-	
-		rb = GetComponent <Rigidbody> ();
-
+		cg = GameObject.Find ("Camera").GetComponent<Camara_giros> ();
+		rg = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		Acelera ();
-
-		GiraDch ();
-
-		GiraIzq ();
-	
-			
-
-	
 	}
-
-
-	void Acelera () {
-
-		if (Input.GetKey (KeyCode.W)) {
-			rb.AddForce (transform.forward * accel);
+	void MovimientoIzq(){
+		Debug.Log (cg.transform.rotation.z);
+		if (cg.transform.rotation.z <= 90 ) {
+			Debug.Log("giro izq");
+			rg.transform.Rotate  (izq);
 		}
 	}
+	void MovimientoDrcha(){}
 
-
-	void GiraIzq () {
-		if (Input.GetKey (KeyCode.A)) {
-			rb.transform.Rotate (Vector3.right, -giro * Time.deltaTime);
-			
-		}
-	}
-
-	void GiraDch () {
-		if (Input.GetKey (KeyCode.A)) {
-			rb.transform.Rotate (Vector3.right, giro * Time.deltaTime);
-			
-		}
-
-}
 }
---
+++ LosCajas/Los cajas/Assets/Scripts/pruebas/Movimiento.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+	public float accel = 100f;
+	Rigidbody rb;
+	public float giro;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent <Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Acelera ();
+
+		GiraDch ();
+
+		GiraIzq ();
+	
+			
+
+	
+	}
+
+
+	void Acelera () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddForce (transform.forward * accel);
+		}
+	}
+
+
+	void GiraIzq () {
+		if (Input.GetKey (KeyCode.A)) {
+			rb.transform.Rotate (Vector3.right, -giro * Time.deltaTime);
+			
+		}
+	}
+
+	void GiraDch () {
+		if (Input.GetKey (KeyCode.A)) {
+			rb.transform.Rotate (Vector3.right, giro * Time.deltaTime);
+			
+		}
+
+}
+}
