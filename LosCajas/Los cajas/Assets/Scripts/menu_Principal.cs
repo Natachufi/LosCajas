85cd1e3786fdfeeefec0207b07ea58a513b4f33a
Natasha Molins Miralles <natashamolins@gmail.com>
2016-02-23 19:06:08 +0100

    prefab menu_principal

---
+++ LosCajas/Los cajas/Assets/Scripts/menu_Principal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu_Principal : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void grande() { //cuando el boton se hace grande
+		anim.SetBool("mirando",true);
+	
+	}
+	public void pequeno(){ //cuando el boton vuelve a su tamaño original
+		anim.SetBool("mirando", false);
+	}
+}
