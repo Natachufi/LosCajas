a370864eb2a8ff6081e4e2633febf8a6c0b28ef9
Natasha Molins Miralles <natashamolins@gmail.com>
2016-02-23 20:12:04 +0100

    scripts individuales para los botones del menu principal

--- LosCajas/Los cajas/Assets/Scripts/principal_creditos.cs
+++ LosCajas/Los cajas/Assets/Scripts/principal_creditos.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class menu_Principal : MonoBehaviour {
+public class principal_creditos : MonoBehaviour {
+
 	Animator anim;
 	public float tiempo_mirada = 3f;
 	float tiempo = 0;
 	bool mirando = false;
-
+	
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
@@ -17,20 +18,20 @@ public class menu_Principal : MonoBehaviour {
 		if(mirando && tiempo < Time.time  ){
 			Debug.Log ( "miau");
 		}
-	
+		
 	}
-
+	
 	public void grande() { //cuando el boton se hace grande
 		anim.SetBool("mirando",true);
 		mirando = true;
 		tiempo = Time.time + tiempo_mirada; 
 		Debug.Log ( "grande");
-	
+		
 	}
 	public void pequeno(){ //cuando el boton vuelve a su tamaño original
 		anim.SetBool("mirando", false);
 		mirando = false;
 		Debug.Log ( "pequeño");
-
+		
 	}
 }
