6722ebb5dbd3b3aed8dcad657516881fd6963482
Natasha Molins Miralles <natashamolins@gmail.com>
2016-02-23 20:03:21 +0100

    Contador de menu_principal y animaciones bien

--- LosCajas/Los cajas/Assets/Scripts/menu_Principal.cs
+++ LosCajas/Los cajas/Assets/Scripts/menu_Principal.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class menu_Principal : MonoBehaviour {
 	Animator anim;
+	public float tiempo_mirada = 3f;
+	float tiempo = 0;
+	bool mirando = false;
 
 	// Use this for initialization
 	void Start () {
@@ -11,14 +14,23 @@ public class menu_Principal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(mirando && tiempo < Time.time  ){
+			Debug.Log ( "miau");
+		}
 	
 	}
 
 	public void grande() { //cuando el boton se hace grande
 		anim.SetBool("mirando",true);
+		mirando = true;
+		tiempo = Time.time + tiempo_mirada; 
+		Debug.Log ( "grande");
 	
 	}
 	public void pequeno(){ //cuando el boton vuelve a su tamaño original
 		anim.SetBool("mirando", false);
+		mirando = false;
+		Debug.Log ( "pequeño");
+
 	}
 }
