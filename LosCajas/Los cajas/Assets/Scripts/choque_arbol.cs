e1ea4ca9b96ebef23147fe5c71e0a564029da03f
alberto <afp.forni@gmail.com>
2016-02-24 18:18:30 +0100

    [59] programacion sonido choque
    tambien subo el prefab del arbol

---
+++ LosCajas/Los cajas/Assets/Scripts/choque_arbol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+
+public class choque_arbol : MonoBehaviour {
+
+	private AudioSource sonido;
+
+	// Use this for initialization
+	void Start () {
+		sonido = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void OnCollisionStay (Collision objeto){
+		if (objeto.transform.tag == "Player") {
+			sonido.Play ();
+	
+		}
+	}
+	
+
+		
+	
+
+	void Update () {
+
+	
+	}
+}
