cab84c84601d56bc16675cd89004be234284636c
Natasha Molins Miralles <natashamolins@gmail.com>
2016-02-24 17:54:33 +0100

    #71 Animación y scripts

---
+++ LosCajas/Los cajas/Assets/Scripts/barra_tiempo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barra_tiempo : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void activado(){
+		gameObject.SetActive(true);
+		anim.Play("barra_segundos");
+		
+	}
+	public void desactivado(){ 
+		gameObject.SetActive(false);
+		
+	}
+
+
+}
