c220ffafe5d1ae3277358aac859b5519ba142d85
Torrehon <albtorpa93@gmail.com>
2016-03-01 18:54:20 +0100

    MenuScoreProgramado

---
+++ LosCajas/Los cajas/Assets/Score.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public Animator score;
+	public Animator banderolas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	
+	}
+
+	void OnTriggerEnter (Collider objeto) {
+		    
+			Debug.Log ("entra");
+
+		if (objeto.transform.tag == "Player") {
+
+
+			score.SetTrigger ("entra");
+			banderolas.SetTrigger ("entran");
+
+		}
+	}
+
+
+}
